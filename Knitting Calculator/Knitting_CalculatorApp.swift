9aedea14e33a7b9aaed3a327d4b87dc1a4207427
Debbie <dengelmeyer@icloud.com>
2024-03-10 14:34:38 -0700

    Initial Commit

---
+++ Knitting Calculator/Knitting_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Knitting_CalculatorApp.swift
+//  Knitting Calculator
+//
+//  Created by Deborah Engelmeyer on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Knitting_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
